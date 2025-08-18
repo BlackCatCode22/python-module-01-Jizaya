20716659790583f2c4de8bed5355a1d631ec0a6c
Jizaya <104530606+Jizaya@users.noreply.github.com>
2025-08-17 20:50:15 -0700

    Chapter 3 Examples

---
+++ conditional_execution.py
@@ -0,0 +1,16 @@
+x = 3
+y = 2
+
+if x > 0:
+    print('x is positive')
+
+if x > y:
+    print(x)
+    print(y)
+
+if x < 0:
+    pass # need to handle negative values, do nothing for now.
+
+
+if x < 10:
+    print('Small')
